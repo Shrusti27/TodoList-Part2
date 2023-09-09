ddf821231e1f76b8a2ede93dbe58c96a8c779256
Shrusti98 <Shrustimalviya98@gmail.com>
2023-09-09 16:54:51 +0530

    first commit

---
+++ todolist/pages/index.js
@@ -0,0 +1,83 @@
+"use client"
+import React, { useState } from 'react'
+
+const index = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
+  const [mainTask, setsMainTask] = useState([])
+
+  const submitHandler=(e) =>{
+    e.preventDefault() //
+    setsMainTask([...mainTask, {title,desc}]);
+
+    console.log(mainTask)
+    console.log(title)
+    console.log(desc)
+    settitle("")
+    setdesc("")
+  };
+
+  const deleteHandler=(i)=>{
+    let copytask=[...mainTask]
+    copytask.splice(i,1)
+    setsMainTask(copytask)
+  }
+
+
+  let renderTask= <h2>No Task Available</h2>
+
+  if(mainTask.length>0){
+
+  renderTask=mainTask.map((t,i) => {
+    return (
+      <li key={i} className='flex items-center justify-between mb-8'>
+        <div className='flex items-center  justify-between w-2/3'>
+          <h5 className='text-2xl font-semibold'>{t.title}</h5>
+          <h6 className='text-lg font-medium'>{t.desc}</h6>
+        </div>
+
+        <button 
+        onClick={() =>{
+          deleteHandler(i)
+        }}
+        className='bg-red-400 text-white px-4 py-2 rounded font-bold '>Delete</button>
+      </li>
+    );
+  });
+}
+
+  return (
+    <>
+    <h1 className='bg-black text-white font-bold p-5 text-4xl text-center'> My TodoList</h1>
+
+    <form onSubmit={submitHandler}>
+      <input type='text' className='text-2xl border-zinc-800 border-2 m-8 px-4 py-2 rounded m-5'
+      placeholder='Enter your title here'
+      value={title}
+      onChange={(e)=>{
+      settitle(e.target.value)
+      }}
+      />
+
+    <input type='text' className='text-2xl border-zinc-800 border-2 m-8 px-4 py-2 rounded m-5'
+      placeholder='Enter Description here'
+      value={desc}
+      onChange={(e)=>{
+        setdesc(e.target.value)
+      }}
+      />
+
+      <button className='bg-black text-white px-4 py-2 text-2xl rounded m-5'>Add Task</button>
+    </form>
+    <hr/>
+ 
+    <div className='p-8 bg-slate-200'>
+      <ul>
+        {renderTask}
+      </ul>
+    </div>
+    </>
+  );
+};
+
+export default index
